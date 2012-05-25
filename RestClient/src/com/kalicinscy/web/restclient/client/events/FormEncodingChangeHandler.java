a8414173b4627dcf9941501cfb396634750f4d5b
Paweł Psztyć <jarrodek@gmail.com>
2012-05-25 19:04:39 +0000

    Obsolite.
    git-svn-id: https://chrome-rest-client.googlecode.com/svn/trunk@120 4310ba71-df81-c583-98c1-67588e199800
    
    Former-commit-id: dd2a91e7fbfbdf7b006328d14f1e80cbc4fcef5f [formerly 7501f679d2b1c4f55f9c68988fc1507782277229]
    Former-commit-id: d56749dfba15f00b250204a5e5c091d1270e8bfe

--- RestClient/src/com/kalicinscy/web/restclient/client/events/FormEncodingChangeHandler.java
+++
@@ -1,7 +0,0 @@
-package com.kalicinscy.web.restclient.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface FormEncodingChangeHandler extends EventHandler {
-	void onEncodingChange(FormEncodingChangeEvent event);
-}
--- RestClient/src/com/kalicinscy/web/restclient/client/events/MethodChangedHandler.java
+++
@@ -1,7 +0,0 @@
-package com.kalicinscy.web.restclient.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface MethodChangedHandler extends EventHandler {
-	void onMethodChange(MethodChangedEvent event);
-}
