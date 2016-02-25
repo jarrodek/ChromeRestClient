9537e6126fabb6f0812540e86105a45034aa631e
Paweł Psztyć <jarrodek@gmail.com>
2016-02-25 14:41:13 +0000

    Fixed install script on osx

--- bin/install.js
+++ bin/install.js
@@ -2,28 +2,48 @@
 var shell = require('shelljs');
 
 var chromeCmd = '';
+var osx = false;
 switch (process.platform) {
   case 'linux':
   case 'freebsd':
+  case 'sunos':
     chromeCmd = 'google-chrome';
     break;
-  case 'sunos':
-    chromeCmd = '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome';
+  case 'darwin':
+    osx = true;
     break;
   case 'win32':
     chromeCmd = 'chrome.exe';
     break;
   default:
     shell.echo('Unsupported platform');
-    shell.exit(1);
+    shell.exit(0);
     break;
 };
-var chromeExec = shell.which(chromeCmd);
-if (!chromeExec) {
-  shell.echo('This script requires Google chrome to be installed.');
-  shell.exit(1);
+if (!osx) {
+  isInstalled(chromeCmd);
+  runApp(chromeCmd);
+} else {
+  runAppOsx();
 }
-var currentDirectory = shell.pwd();
-var cmd = `${chromeCmd} --silent-launch --load-and-launch-app="${currentDirectory}" &`;
 
-shell.exec(cmd);
+function isInstalled(program) {
+  var chromeExec = shell.which(program);
+  if (!chromeExec) {
+    shell.echo('This script requires Google chrome to be installed.');
+    shell.exit(0);
+  }
+}
+
+function runApp(program) {
+  var currentDirectory = shell.pwd();
+  var cmd = `${chromeCmd} --silent-launch --load-and-launch-app="${currentDirectory}" &`;
+  shell.exec(cmd);
+}
+
+function runAppOsx() {
+  var currentDirectory = shell.pwd();
+  var cmd = 'open /Applications/Google\\ Chrome.app --args --silent-launch ';
+  cmd += `--load-and-launch-app="${currentDirectory}" &`;
+  shell.exec(cmd);
+}
