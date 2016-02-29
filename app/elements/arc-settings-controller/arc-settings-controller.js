2179a32e3d7c99f6fda0081b2daa7f0980378996
jarrodek <jarrodek@gmail.com>
2016-02-29 17:43:47 +0000

    Removed old settings options not used in app anymore

--- app/elements/arc-settings-controller/arc-settings-controller.js
+++ app/elements/arc-settings-controller/arc-settings-controller.js
@@ -53,9 +53,7 @@ Polymer({
    */
   _onStorageChanged: function(changes) {
     var keys = Object.keys(changes);
-    var accepted = ['DEBUG_ENABLED', 'HISTORY_ENABLED', 'MAGICVARS_ENABLED',
-      'CMH_ENABLED', 'CMP_ENABLED'
-    ];
+    var accepted = ['HISTORY_ENABLED', 'MAGICVARS_ENABLED'];
     keys.forEach(function(key) {
       if (accepted.indexOf(key) !== -1 && this.values[key] !== changes[key].newValue) {
         this.set('values.' + key, changes[key].newValue);
