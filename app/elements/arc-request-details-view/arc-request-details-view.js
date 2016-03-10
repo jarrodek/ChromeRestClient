de466f3f1ece0e74f4c33f520c87479b11ba5d0c
jarrodek <jarrodek@gmail.com>
2016-03-10 02:45:19 +0000

    Fixed request details view in saved/history list

--- app/elements/arc-request-details-view/arc-request-details-view.js
+++ app/elements/arc-request-details-view/arc-request-details-view.js
@@ -2,6 +2,7 @@ Polymer({
   is: 'arc-request-details-view',
   properties: {
     request: Object,
+    isHistory: Boolean,
     _hasPayload: {
       type: Boolean,
       value: false,
@@ -18,8 +19,16 @@ Polymer({
   },
 
   _computeHeaders: function(request) {
-    var hd = request.har.entries[0].request.headers;
-    if (!hd) {
+    var entries = request.har.entries;
+    var hd;
+    if (entries && entries.length > 0) {
+      let index = 0;
+      if (this.isHistory) {
+        index = entries.length - 1;
+      }
+      hd = request.har.entries[index].request.headers;
+    }
+    if (!hd || hd.length === 0) {
       return '(no headers)';
     }
     var res = '';
