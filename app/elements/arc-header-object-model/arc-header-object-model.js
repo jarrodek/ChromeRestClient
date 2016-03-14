0d4bba799cab6cf27666a27fe70631706ad0fb25
Pawel Psztyc <jarrodek@gmail.com>
2016-03-14 18:34:42 +0000

    Fixed headers database query

--- app/elements/arc-header-object-model/arc-header-object-model.js
+++ app/elements/arc-header-object-model/arc-header-object-model.js
@@ -5,6 +5,38 @@ Polymer({
   ],
 
   getObject: function() {
-    return this.genericGetObject('headers');
+    return arc.app.db.idb.open()
+      .then((db) => {
+        return db.headers
+          .where('key')
+          .equalsIgnoreCase(this.objectId[0])
+          .and((item) => item.type === this.objectId[1])
+          .toArray()
+          // .get(this.objectId)
+          .finally(() => {
+            db.close();
+          });
+      })
+      .then((data) => {
+        if (data && data.length) {
+          data = data[0];
+        } else {
+          data = null;
+        }
+        this.data = data;
+        this.fire('data-ready', {
+          data: data
+        });
+        return data;
+      })
+      .catch((cause) => {
+        console.error(cause);
+        arc.app.analytics.sendException('arc-model::genericGetObject::' +
+          JSON.stringify(cause), false);
+        this.fire('error', {
+          error: cause
+        });
+        throw cause;
+      });
   }
 });
