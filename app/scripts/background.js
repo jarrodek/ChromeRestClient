f8daa201dcb3a6e1b9a3f25a8bb71da2d38ceda2
jarrodek <jarrodek@gmail.com>
2016-03-16 23:02:04 +0000

    The app can open more than one instance at the time

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Listens for the app launching then creates the window.
  *
@@ -9,17 +11,20 @@ chrome.app.runtime.onLaunched.addListener(function(lunchData) {
   if (lunchData && lunchData.id) {
     switch (lunchData.id) {
       case 'google_drive_open':
-        var _url = lunchData.url;
+        let _url = lunchData.url;
         _url = _url.substr(_url.indexOf('state') + 6);
-        var data = JSON.parse(decodeURIComponent(_url));
-        var id = data.ids[0];
+        let data = JSON.parse(decodeURIComponent(_url));
+        let id = data.ids[0];
         url += '#!/request/drive/' + id;
       break;
     }
   }
+  var all = chrome.app.window.getAll();
+  var length = all.length;
+  var id = 'arc-window-' + length;
   chrome.app.window.create(
     url, {
-      id: 'arc-window',
+      id: id,
       bounds: {
         width: 800,
         height: 600
