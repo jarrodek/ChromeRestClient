e5c97f9aa2d4d18bdec0b960ee98b6e79452f88f
Paweł Psztyć <jarrodek@gmail.com>
2016-03-22 13:54:58 +0000

    Added button to apps FAQ to not signed in error view

--- app/elements/chrome-not-signedin-view/chrome-not-signedin-view.js
+++ app/elements/chrome-not-signedin-view/chrome-not-signedin-view.js
@@ -3,5 +3,9 @@ Polymer({
 
   close: function() {
     this.fire('cancel');
+  },
+
+  _learnMore: function() {
+    window.open('https://github.com/jarrodek/ChromeRestClient/wiki/faq#signing-in-and-permissions');
   }
 });
