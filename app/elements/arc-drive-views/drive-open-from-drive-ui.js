5ba11188704e3948ef30a550a7768ddb7ba8604c
Paweł Psztyć <jarrodek@gmail.com>
2016-03-22 17:24:17 +0000

    Fixes #397

--- app/elements/arc-drive-views/drive-open-from-drive-ui.js
+++ app/elements/arc-drive-views/drive-open-from-drive-ui.js
@@ -3,5 +3,9 @@ Polymer({
   is: 'drive-open-from-drive-ui',
   _back: function() {
     this.fire('restore-view');
+  },
+
+  _openDrive: function() {
+    window.open('https://drive.google.com');
   }
 });
