e01f3147a5f7607493006b1175c36fd8693591a3
Pawel Psztyc <jarrodek@gmail.com>
2016-04-28 17:01:41 +0100

    Fixes #628 - Cookie path matching

--- tasks/ssl.js
+++ tasks/ssl.js
@@ -217,7 +217,51 @@ class TestServer {
     });
   }
 
+  uuid() {
+    // jscs:disable
+    /* jshint ignore:start */
+    var lut = [];
+    for (var i = 0; i < 256; i++) {
+      lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
+    }
+    var fn = function() {
+      var d0 = Math.random() * 0xffffffff | 0;
+      var d1 = Math.random() * 0xffffffff | 0;
+      var d2 = Math.random() * 0xffffffff | 0;
+      var d3 = Math.random() * 0xffffffff | 0;
+      return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] +
+        lut[d0 >> 24 & 0xff] + '-' + lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' +
+        lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' + lut[d2 & 0x3f | 0x80] +
+        lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
+        lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
+    };
+    return fn();
+    // jscs:enable
+    /* jshint ignore:end */
+  }
+
   _setCookie() {
+    app.get('/store/CatalogActor/getProduct', (req, res) => {
+      var cookie = null;
+      var regenerate = false;
+      if (!req.cookies.JSESSIONID) {
+        cookie = this.uuid();
+        regenerate = true;
+      } else {
+        cookie = req.cookies.JSESSIONID;
+      }
+      res.cookie('JSESSIONID', cookie, {
+        path: '/store'
+      });
+      res.set('Content-Type', 'text/html');
+      var message = '<h1>Session cookie set.</h1>';
+      if (regenerate) {
+        message += '<p>This cookie has ben generated as new</p>';
+      } else {
+        message += '<p>This cookie originates from previous session</p>';
+      }
+      res.send(message);
+    });
     //set random cookies
     app.get('/cookies/random', (req, res) => {
       var Chance = require('chance');
