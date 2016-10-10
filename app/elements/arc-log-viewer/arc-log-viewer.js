ff67209cec906d0f8cf3b37630623e55183d1d21
jarrodek <jarrodek@gmail.com>
2016-10-10 14:31:05 +0100

    Update: Updated log viewer in the app

--- app/elements/arc-log-viewer/arc-log-viewer.js
+++ app/elements/arc-log-viewer/arc-log-viewer.js
@@ -4,37 +4,22 @@ Polymer({
   is: 'arc-log-viewer',
 
   properties: {
-    logs: Array,
-    details: Object,
-    showDetails: {
-      type: Boolean,
-      value: false
-    },
-    levelLog: {
-      type: Boolean,
-      value: true,
-      observer: '_computeFilterArray'
-    },
-    levelInfo: {
-      type: Boolean,
-      value: true,
-      observer: '_computeFilterArray'
-    },
-    levelWarning: {
-      type: Boolean,
-      value: true,
-      observer: '_computeFilterArray'
-    },
-    levelError: {
-      type: Boolean,
-      value: true,
-      observer: '_computeFilterArray'
-    },
-    allowedLogs: {
+    logs: {
       type: Array,
       value: function() {
         return [];
       }
+    },
+    details: Object,
+    // Either 0 (list of logs) or 1 (log details)
+    viewId: {
+      type: Number,
+      value: 0
+    },
+    // Number of records per page.
+    pageLimit: {
+      type: Number,
+      value: 30
     }
   },
 
@@ -53,17 +38,22 @@ Polymer({
 
   updateLogs: function(opened) {
     if (!opened) {
-      this.set('logs', []);
-      this.queryOptions = undefined;
+      this.reset();
       return;
     }
     this._getLogs();
   },
 
+  reset: function() {
+    this.set('logs', []);
+    this.queryOptions = undefined;
+    this.noMoreLogs = false;
+  },
+
   _getLogs: function() {
     if (!this.queryOptions) {
       this.queryOptions = {
-        limit: 25,
+        limit: this.pageLimit,
         descending: true,
         // jscs:disable
         include_docs: true
@@ -75,11 +65,17 @@ Polymer({
     .then((response) => {
       var items = [];
       if (response && response.rows.length > 0) {
-        this.queryOptions.startkey = response.rows[response.rows.length - 1];
+        this.queryOptions.startkey = response.rows[response.rows.length - 1].key;
         this.queryOptions.skip = 1;
         items = response.rows;
       } else {
         items = [];
+      }
+      if (items.length === 0) {
+        this.noMoreLogs = true;
+        return;
+      } else if (items.length < this.pageLimit) {
+        this.noMoreLogs = true;
       }
       items = items.concat(this.logs);
       this.set('logs', items);
@@ -93,7 +89,7 @@ Polymer({
   },
 
   hideDetails: function() {
-    this.showDetails = false;
+    this.viewId = 0;
     this.notifyResize();
   },
 
@@ -139,42 +135,11 @@ Polymer({
       return;
     }
     this.set('details', item.doc);
-    this.showDetails = true;
-  },
-
-  _computeFilterArray: function() {
-    var result = [];
-    if (this.levelLog) {
-      result.push('log');
-    }
-    if (this.levelInfo) {
-      result.push('info');
-    }
-    if (this.levelWarning) {
-      result.push('warning');
-    }
-    if (this.levelError) {
-      result.push('error');
-    }
-    this.allowedLogs = result;
-
-    if (this.opened) {
-      this._refreshLogs();
-    }
-  },
-
-  sortList: function(a, b) {
-    if (a.time > b.time) {
-      return -1;
-    }
-    if (a.time < b.time) {
-      return 1;
-    }
-    return 0;
+    this.viewId = 1;
   },
 
   _refreshLogs: function() {
-    this.set('logs', []);
+    this.reset();
     this._getLogs();
   },
 
