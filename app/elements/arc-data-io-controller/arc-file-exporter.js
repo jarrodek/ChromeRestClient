1c418af3a68434377d46c8d93c1841a54ed8efa8
CI Agent <arc@mulesoft.com>
2017-01-19 16:01:02 +0000

    New: Add option to export data to Google Drive

--- app/elements/arc-data-io-controller/arc-file-exporter.js
+++ app/elements/arc-data-io-controller/arc-file-exporter.js
@@ -7,47 +7,78 @@ Polymer({
     ArcBehaviors.ArcFileExportBehavior
   ],
   properties: {
-    /** True if user started exporting data to a file. */
+    /** True if user started exporting data to Google Drive. */
     exporting: {
-      notify: true,
-      readOnly: true,
       type: Boolean,
       value: false
     },
-    /** True if data are preparing to export */
-    loading: {
-      notify: false,
-      readOnly: true,
-      type: Boolean,
-      value: false
-    },
-    /** `true` when the data is ready */
-    dataReady: {
-      notify: true,
-      readOnly: true,
-      type: Boolean,
-      value: false
-    },
+
     _writableContent: Object,
-    _fileSuggestedName: String
+    _fileSuggestedName: String,
+
+    // Selected form page.
+    selectedPage: {
+      type: Number,
+      value: 0
+    },
+    // True if data are being prepared.
+    preparing: {
+      type: Boolean,
+      value: false
+    },
+    // True when switching to the drive export and the app is checking for the authentication.
+    checkingAuth: {
+      type: Boolean,
+      value: false
+    },
+    // True if the authentication check returns true.
+    authenticated: {
+      type: Boolean,
+      value: false
+    },
+  },
+
+  listeners: {
+    'chrome-signin-aware-success': '_driveAuthSuccess',
+    'chrome-signin-aware-canceled': '_driveAuthNot',
+    'chrome-signin-aware-error': '_driveAuthNot'
+  },
+
+  reset: function() {
+    this.exportContent = undefined;
+    this.fileSuggestedName = undefined;
+    this.selectedPage = 0;
+    this.preparing = false;
+    this.checkingAuth = false;
+    this.authenticated = false;
+    this.exporting = false;
   },
   /**
    * A handler for click on a "prepare data" button.
    */
   _prepareDataClick: function() {
-    this._setExporting(true);
-    this._setLoading(true);
-    this._setDataReady(false);
-    this.debounce('export-click', function() {
-      this._prepareData();
+    var clear = this.$$('#exportForm').serialize();
+    var dbs = Object.keys(clear);
+    if (dbs.length === 0) {
+      StatusNotification.notify({
+        message: 'Select data to be exported.'
+      });
+      return;
+    }
+    if (dbs.length === 8) {
+      dbs = 'all';
+    }
+    // Don't put this before data serialization!
+    this.preparing = true;
 
+    this.debounce('export-click', function() {
+      this._prepareData(dbs);
       this.fire('send-analytics', {
         type: 'event',
         category: 'Settings usage',
         action: 'Export data',
         label: 'Generate file'
       });
-
     }, 50);
   },
   /**
@@ -55,16 +86,17 @@ Polymer({
    * This is an async operation since data preparation may take long time
    * (depending on number of entries).
    */
-  _prepareData: function() {
+  _prepareData: function(type) {
     arc.app.importer.prepareExport({
-      type: 'all' // dump all data available.
+      type: type
     })
-    .then(function(data) {
+    .then((data) => {
       this.exportContent = data;
-      this._setLoading(false);
-      this._setDataReady(true);
-    }.bind(this))
+      this.selectedPage = 1;
+      this.preparing = false;
+    })
     .catch((cause) => {
+      this.preparing = false;
       this.fire('app-log', {'message': cause, 'level': 'error'});
       StatusNotification.notify({
         message: cause.message
@@ -76,31 +108,88 @@ Polymer({
       });
     });
   },
-  _saveData: function() {
+
+  _suggestedFilename: function() {
     var date = new Date();
     var day = date.getDate();
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
-    this.fileSuggestedName = 'arc-export-' + day + '-' + month + '-' + year + '-export.json';
+    return 'arc-export-' + day + '-' + month + '-' + year + '-export.json';
+  },
+
+  _saveData: function() {
+    this.fileSuggestedName = this._suggestedFilename();
     this.exportMime = 'json';
     this.exportData();
   },
+  /**
+   * Starts the export to drive flow.
+   * It checks the authotization status. If the user didn't authorized the application then
+   * it displays an authorization form. Otherwise the file name form is displayed.
+   */
+  _exportDriveFlow: function() {
+    this.selectedPage = 2;
+    this.checkingAuth = true;
+    this.fileSuggestedName = this._suggestedFilename();
+    window.drive.isAuth()
+    .then((authenticated) => {
+      this.checkingAuth = false;
+      this.authenticated = authenticated;
+    })
+    .catch((e) => {
+      this.reset();
+      StatusNotification.notify({
+        message: 'Authentication error. ' + e.message
+      });
+    });
+  },
+
+  _exportDataDrive: function() {
+    if (!this.fileSuggestedName) {
+      StatusNotification.notify({
+        message: 'Enter exported file name first '
+      });
+      return;
+    }
+    this.exporting = true;
+    /* global drive */
+    drive.file.create({
+      resource: {
+        name: this.fileSuggestedName,
+        description: 'Advanced REST client backup file.',
+        properties: {
+          isExport: true,
+          version: arc.app.utils.appVer
+        }
+      },
+      media: {
+        mimeType: 'application/json',
+        body: JSON.stringify(this.exportContent)
+      }
+    })
+    .then(() => {
+      this.reset();
+      StatusNotification.notify({
+        message: 'Data export to Google Drive completed'
+      });
+    })
+    .catch((e) => {
+      this.exporting = false;
+      StatusNotification.notify({
+        message: 'Google Drive export error. ' + e.message
+      });
+    });
+  },
 
   onFileSaved: function() {
-    this.exportContent = null;
-    this._setDataReady(false);
-    this._setLoading(false);
-    this._setExporting(false);
+    this.reset();
     StatusNotification.notify({
-      message: 'Data exported'
+      message: 'Data export completed'
     });
   },
 
   onFileError: function(e) {
-    this.exportContent = null;
-    this._setDataReady(true);
-    this._setLoading(false);
-    this._setExporting(true);
+    this.reset();
     if (e.detail.message.indexOf('cancelled') !== -1) {
       return;
     }
@@ -116,15 +205,15 @@ Polymer({
   },
 
   _cancelExport: function() {
-    this._setDataReady(false);
-    this._setLoading(false);
-    this._setExporting(false);
-    this._writableContent = null;
-    this._fileSuggestedName = null;
+    this.reset();
   },
 
-  _computeDisplaySelector: function(loading, exporting) {
-    return !loading && !exporting;
+  _driveAuthSuccess: function() {
+    this.authenticated = true;
+  },
+
+  _driveAuthNot: function() {
+    this.authenticated = false;
   }
 });
 })();
