354a974a56a7c4e1f8193eee2f769964af9998ae
Pawel Psztyc <jarrodek@gmail.com>
2019-03-11 20:33:13 -0700

    Cleaning project

--- tasks/crisper-bower.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const crisper = require('gulp-crisper');
-gulp.src('app/bower_components/**/*.html')
-.pipe(crisper({
-  scriptInHead: false,
-  onlySplit: false,
-  alwaysWriteScript: false
-}))
-.pipe(gulp.dest('app/bower_components/'));
--- wct.conf.js
+++ wct.conf.js
@@ -1,11 +1,10 @@
 #! /usr/bin/env node
+const path = require('path');
 
-var path = require('path');
-
-var ret = {
-  'verbose': true,
-  'suites': ['app/test'],
-  'webserver': {
+const ret = {
+  verbose: true,
+  suites: ['app/test'],
+  webserver: {
     'pathMappings': []
   },
   plugins: {
@@ -15,10 +14,9 @@ var ret = {
   }
 };
 
-var mapping = {};
-var rootPath = (__dirname).split(path.sep).slice(-1)[0];
-mapping['/components/' + rootPath  + '/app/bower_components'] = 'bower_components';
+const mapping = {};
+const rootPath = (__dirname).split(path.sep).slice(-1)[0];
+mapping['/components/' + rootPath + '/app/bower_components'] = 'bower_components';
 ret.webserver.pathMappings.push(mapping);
 console.log(ret.webserver.pathMappings[0]);
 module.exports = ret;
-//{ '/components/ChromeRestClient/app/bower_components': 'bower_components' }
